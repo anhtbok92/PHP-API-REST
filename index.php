62f6b48d0a0e0dac053a7a45994f3c1ee3d00df4
Tuan Anh <victory1080@gmail.com>
2021-03-16 01:32:35 +0700

    add logic get price of company

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+define("COMPANY_API", 'https://5f27781bf5d27e001612e057.mockapi.io/webprovise/companies');
+define("TRAVEL_API", 'https://5f27781bf5d27e001612e057.mockapi.io/webprovise/travels');
+
 class Common
 {
     public function clientGet($urlEndPoint) {
@@ -30,51 +33,101 @@ class Common
             'response' => $response
         ];
     }
+
+    public function groupBy($array, $key) {
+        $return = array();
+        foreach($array as $val) {
+            $return[$val[$key]][] = $val;
+        }
+        return $return;
+    }
 }
+
 class Travel
 {
     protected $common;
-    protected $URL = 'https://5f27781bf5d27e001612e057.mockapi.io/webprovise/travels';
+    protected $URL = TRAVEL_API;
     public function __construct() {
         $this->common = new Common();
     }
 
     public function getTravels() {
-        return $this->common->clientGet($this->URL);
+        return json_decode($this->common->clientGet($this->URL)['response'], TRUE);
+    }
+
+    public function getTotalPriceTravels() {
+        $travels = $this->getTravels();
+        $groupByTravels = (new Common())->groupBy($travels, 'companyId');
+        $travelPriceByCompany = [];
+        foreach ($groupByTravels as $companyId => $employees) {
+            $travelPriceByCompany[$companyId] = array_sum(array_column($employees, 'price'));
+        }
+
+        return $travelPriceByCompany;
     }
 }
 
 class Company
 {
     protected $common;
-    protected $URL = 'https://5f27781bf5d27e001612e057.mockapi.io/webprovise/companies';
+    protected $URL = COMPANY_API;
     public function __construct() {
         $this->common = new Common();
     }
 
     public function getCompanies() {
-        return $this->common->clientGet($this->URL);
+        return json_decode($this->common->clientGet($this->URL)['response'], TRUE);
+    }
+
+    public function addCost($travelPriceByCompany) {
+        $companies = $this->getCompanies();
+        foreach ($companies as $key => $company) {
+            if ($company['parentId'] == "0") {
+                $companies[$key]['parentId'] = $company['id'];
+            }
+            $companies[$key]['cost'] = $travelPriceByCompany[$company['id']];
+        }
+
+        return $companies;
     }
 }
 
 class TestScript
 {
+    public function getCompanyInfo($companies, $id) {
+        return array_filter($companies, function($company) use($id){
+            return $company['id'] == $id;
+        });
+    }
+
+    public function removeCurrentCompany($companies, $id) {
+        foreach ($companies as $key => $company) {
+            if ($company['id'] == $id) {
+                unset($companies[$key]);
+            }
+        }
+        return $companies;
+    }
+
     public function execute()
     {
         $start = microtime(true);
-        // Enter your code here
-        $travels = (new Travel())->getTravels();
-        $companies = (new Company())->getCompanies();
-        var_dump($companies);
 
-        // Get parent company
+        $travelPriceByCompany = (new Travel())->getTotalPriceTravels();
+        $companies = (new Company())->addCost($travelPriceByCompany);
+        $groupByCompanies = (new Common())->groupBy($companies, 'parentId');
 
+        $result = [];
+        foreach ($groupByCompanies as $companyId => $companyChild) {
+            $result[] = [
+                'id' => $companyId,
+                'name' => $this->getCompanyInfo($companies, 'uuid-1')[0]['name'],
+                'cost' => array_sum(array_column($companyChild, 'cost')),
+                'children' => $this->removeCurrentCompany($companyChild, $companyId)
+            ];
+        }
 
-        // Push child company to parent company
-
-
-
-        // (new TestScript())->execute();
+        print_r($result);
         echo 'Total time: '.  (microtime(true) - $start). "\n";
     }
 }
